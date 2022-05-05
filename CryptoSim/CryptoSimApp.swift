50a7f626736a5abe8997527164be767feae481ed
Pat Butler <15131675+Beefycoder@users.noreply.github.com>
2022-05-05 14:15:35 -0700

    Initial Commit

---
+++ CryptoSim/CryptoSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoSimApp.swift
+//  CryptoSim
+//
+//  Created by Pat Butler on 2022-05-05.
+//
+
+import SwiftUI
+
+@main
+struct CryptoSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
